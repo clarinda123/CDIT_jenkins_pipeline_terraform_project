832b79097691a7b77522fba50f09c2156686d5a6
Clarinda123 <engonweitiddingclarinda@gmail.com>
2024-04-04 01:45:25 +0100

    updated files

--- environments/env_qa/output.tf
+++ environments/env_qa/output.tf
@@ -1,53 +1,53 @@
 #vpc id
-output "jp_vpc_dev_id" {
+output "jp_vpc_qa_id" {
     value = module.networking.jp_vpc_id
 }
-# internet_gateway_dev_id
-output "igw_id" {
+# internet gateway id
+output "igw_qa_id" {
     value = module.networking.igw_id
   
 }
-# pub_subnet_1a_dev_id
-output "pub_sn_1a_id" {
+# public subnet 1a id
+output "pub_sn_1a_qa_id" {
     value = module.networking.pub_sn_1a_id
 }
-# pub_subnet_1b_dev_id
-output "pub_sn_1b_id" {
+# public subnet 1b id
+output "pub_sn_1b_qa_id" {
     value = module.networking.pub_sn_1b_id
 }
-# priv_app_subnet_1a_dev_id
-output "priv_app_sn_1a_id" {
+# private app subnet 1a id
+output "priv_app_sn_1a_qa_id" {
     value = module.networking.priv_app_sn_1a_id
 }
-# priv_app_subnet_1b_dev_id
-output "priv_app_sn_1b_id" {
+# private app subnet 1b id
+output "priv_app_sn_1b_qa_id" {
     value = module.networking.priv_app_sn_1b_id
 }
-# priv_db_subnet_1a_dev_id
-output "priv_db_sn_1a_id" {
+# private db subnet 1a id
+output "priv_db_sn_1a_qa_id" {
     value = module.networking.priv_db_sn_1a_id
 }
-# priv_db_subnet_1b_dev_id
-output "priv_db_sn_1b_id" {
+# private db subnet 1b id
+output "priv_db_sn_1b_qa_id" {
     value = module.networking.priv_db_sn_1b_id
 }
-# public_route_table_dev_id
-output "pub_rt_id" {
+# public route table id
+output "pub_rt_qa_id" {
     value = module.networking.pub_rt_id
 }
-# private_route_table_1a_dev_id
-output "priv_app_rt_1a_id" {
+# private app route table 1a
+output "priv_app_rt_1a_qa_id" {
     value = module.networking.priv_app_rt_1a_id
 }
-# private_route_table_1b_dev_id
-output "priv_app_rt_1b_id" {
+# private app route table 1b
+output "priv_app_rt_1b_qa_id" {
     value = module.networking.priv_app_rt_1b_id
 }
-# nat_gateway_1a_dev_id
-output "ngw_1a_id" {
+# nat gateway 1a id
+output "ngw_1a_qa_id" {
     value = module.networking.ngw_1a_id
 }
-# nat_gateway_1b_dev_id
-output "ngw_1b_id" {
+# nat gateway 1b id
+output "ngw_1b_qa_id" {
     value = module.networking.ngw_1b_id
 }
